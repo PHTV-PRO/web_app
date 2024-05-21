3ac75efd518f2d5c190bc7a7fe8bb5ba7c67e3f7
phuslee <115574308+phuslee24@users.noreply.github.com>
2024-05-22 02:01:26 +0700

    up
    finish login to dashboard

---
+++ src/util/settings/config.js
@@ -0,0 +1,8 @@
+
+
+// export const DOMAIN = 'https://movieapi.cyberlearn.vn';
+export const DOMAIN = 'http://localhost:8080';
+export const TOKEN = 'accessToken';
+// export const GROUPID = 'GP00';
+
+export const USER_LOGIN = 'USER_LOGIN';
